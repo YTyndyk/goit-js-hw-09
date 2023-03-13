1af45238fb423b05d7e224e304a416994500102b
YTyndyk <114148643+YTyndyk@users.noreply.github.com>
2023-03-13 12:35:48 +0200

    done task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const CHANGE_COLOR_DELAY = 1000;
+let colorId = null;
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const bodyRef = document.querySelector('body');
 
+startBtn.addEventListener('click', onStartBtnClick);
+stopBtn.addEventListener('click', onStopBtnClick);
+stopBtn.disabled = true;
+
+function onStartBtnClick() {
+  colorId = setInterval(() => {
+    bodyRef.style.backgroundColor = getRandomHexColor();
+  }, CHANGE_COLOR_DELAY);
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+}
+function onStopBtnClick() {
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+
+  clearInterval(colorId);
+}
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
